ef7ddd01a8f65b86f7ad711da51ee2480ec76236
LeonEvergrim <dharcleo@umich.edu>
2025-03-30 05:01:16 -0400

    attempt 1

--- lab15.cpp
+++ lab15.cpp
@@ -11,8 +11,20 @@ double factorial(int);
 * Gets and validates lottery info from the user and places it in   *
 * reference parameters referencing variables in the main function. *
 ********************************************************************/
+void getInfo(int&pick1, int &pick2) {
+    do {
+        cout << "How many balls (1-12) are in the pool to pick from? ";
+        cin >> pick1;
 
-
+        if (pick1 < 1 || pick1 > 12) {
+            cout << "Input Error! There must be between 1 and 12 balls.";
+        }
+    } while (pick1 < 1 || pick1 > 12);
+    do {
+        cout << "How many balls (1-7) will be drawn? ";
+        cin >> pick2;
+    } while (pick2 < 1 || pick2 > 12);
+}
 
 /*******************************************************************
 * computeWays                                                      *
@@ -26,6 +38,16 @@ double factorial(int);
 * intermediate result becoming so large that it causes overflow.   *
 ********************************************************************/
 
+double computeWays(int n, int k) {
+    return factorial(k) * factorial(n - k) / factorial(n);
+}
+double fact(int num) {
+    if (num == 0) {
+        return 1;
+    } else if (num > 0) {
+        return num * factorial(num - 1);
+    }
+}
 
 /*******************************************************************
 * factorial                                                        *
